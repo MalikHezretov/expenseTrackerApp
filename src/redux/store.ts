5260925267f3acd8f799de99603cadfd35d6cef5
Malik <malikhezretm@gmail.com>
2022-04-25 03:13:01 +0800

    remove localstorage persist until fixing bug on onrefresh page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,32 +1,7 @@
 // TODO update to configure store
 import { legacy_createStore as createStore } from 'redux'
 import rootReducer from './reducers'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+const store = createStore(rootReducer, devToolsEnhancer({}));
 
-function saveToLocalStorage(state: any) {
-    try {
-      const serialisedState = JSON.stringify(state);
-      localStorage.setItem("persistantState", serialisedState);
-    } catch (e) {
-      console.warn(e);
-    }
-  }
-
-  function loadFromLocalStorage() {
-    try {
-      const serialisedState = localStorage.getItem("persistantState");
-      if (serialisedState === null) return undefined;
-      console.log('serialisedState: ', JSON.parse(serialisedState))
-      return JSON.parse(serialisedState);
-    } catch (e) {
-      console.warn(e);
-      return undefined;
-    }
-  }
-  
-
-
-const store = createStore(rootReducer, loadFromLocalStorage());
-
-store.subscribe(() => saveToLocalStorage(store.getState()));
-
-export default store;
+export default store; 
